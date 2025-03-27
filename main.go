1730dc094939a68bada256c2f68ac5f6269af49c
meruauez <meru20052305@gmail.com>
2025-03-27 20:47:08 +0500

    Update main.go

--- main.go
+++ main.go
@@ -12,18 +12,18 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	// Task Routes
+	
 	r.HandleFunc("/tasks", handlers.GetTasks).Methods("GET")
 	r.HandleFunc("/tasks", handlers.CreateTask).Methods("POST")
 	r.HandleFunc("/tasks/{id}", handlers.GetTaskByID).Methods("GET")
 	r.HandleFunc("/tasks/{id}", handlers.UpdateTask).Methods("PUT")
 	r.HandleFunc("/tasks/{id}", handlers.DeleteTask).Methods("DELETE")
 
-	// User Routes
+	
 	r.HandleFunc("/users", handlers.GetUsers).Methods("GET")
 	r.HandleFunc("/users", handlers.CreateUser).Methods("POST")
 
-	// Project Routes
+	
 	r.HandleFunc("/projects", handlers.GetProjects).Methods("GET")
 	r.HandleFunc("/projects", handlers.CreateProject).Methods("POST")
 
